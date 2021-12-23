16ac504aaac7b0c6bddc57170e8959d90f3f5aeb
203692214 <203692214@qq.com>
2021-12-23 09:50:44 +0800

    tou

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,42 +1,40 @@
 <template>
-<div class="header">
-     <el-row>
-        <el-col :span="20" class="headerlogo">
-          <div class="bg-purple" style="position: relative;">
-            <img src="../assets/培训机构管理.png" alt="无法显示图片"/>
-			<span id="bg-size">培训机构管理</span>
-          </div>
-        </el-col>
-        <el-col :span="4" class="rightsection">
-          <div class="bg-purple-light">
-            <span class="el-dropdown-link userinfo-inner">欢迎管理员</span>
-            <span class="userinfo-inner">退出</span>
-             </div>
-          </div>
-      </el-row>
-</div>
+  <div class="header">
+    <el-row>
+      <el-col :span="20" class="headerlogo">
+        <div class="bg-purple" style="position: relative">
+          <img src="../assets/培训机构管理.png" alt="无法显示图片" />
+          <span id="bg-size">培训机构管理</span>
+        </div>
+      </el-col>
+      <el-col :span="4" class="rightsection">
+        <div class="bg-purple-light">
+          <span class="el-dropdown-link userinfo-inner">欢迎管理员</span>
+          <span class="userinfo-inner">退出</span>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<style>	
-.header	.el-row{
-		background-color: #2d8ab7;
-    font-size: 18px;
-	  height: 60px;
-	}
-	
-.bg-purple img{
-	width:60px;
-	height: 55px;
-
+<style>
+.header .el-row {
+  background-color: #2d8ab7;
+  font-size: 18px;
+  height: 60px;
 }
 
-.bg-purple span{
-	font-size:18px;
-	position: absolute;
+.bg-purple img {
+  width: 60px;
+  height: 55px;
 }
 
-.userinfo-inner{
-	margin-left: 30px;
+.bg-purple span {
+  font-size: 18px;
+  position: absolute;
 }
 
+.userinfo-inner {
+  margin-left: 30px;
+}
 </style>
